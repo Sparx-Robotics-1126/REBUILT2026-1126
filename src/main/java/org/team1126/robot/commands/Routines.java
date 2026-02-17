875e53039dc2313ed97c70ec69b68cc0f78328f0
Steve Buck <steve@softwarily.org>
2026-02-17 09:00:52 -0500

    building pivot framework to base the pose provider off of

--- src/main/java/org/team1126/robot/commands/Routines.java
+++ src/main/java/org/team1126/robot/commands/Routines.java
@@ -10,6 +10,7 @@ import java.util.function.BooleanSupplier;
 import org.team1126.lib.tunable.TunableTable;
 import org.team1126.lib.tunable.Tunables;
 import org.team1126.lib.tunable.Tunables.TunableBoolean;
+import org.team1126.lib.tunable.Tunables.TunableDouble;
 import org.team1126.robot.Robot;
 import org.team1126.robot.subsystems.Lights;
 import org.team1126.robot.subsystems.Swerve;
@@ -25,6 +26,9 @@ public final class Routines {
     private static final TunableTable tunables = Tunables.getNested("routines");
 
     private static final TunableBoolean autoDrive = tunables.value("autoDrive", true);
+    
+    private static final TunableDouble pivotDecel = tunables.value("pivotDecel", 10.0);
+    private static final TunableDouble pivotTol = tunables.value("pivotTol", 0.25);
 
     private final Robot robot;
 
@@ -65,5 +69,9 @@ public final class Routines {
 
     public Command refuelFromDepot() {
         return swerve.apfDrive(() -> new Pose2d(Field.DEPOT_X, Field.DEPOT_Y, Field.DEPOT_ROT), () -> 0.8);
+    }
+    
+    public Command pivot(boolean left) {
+        return swerve.apfDrive(() -> Field.pivot.get(left), pivotDecel::get, pivotTol::get);
     }
 }
