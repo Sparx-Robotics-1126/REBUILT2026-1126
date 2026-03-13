9216a4c28747aeff34cfed6ea052853772b46df8
flareware95 <flareware95@gmail.com>
2026-03-13 13:16:57 -0400

    Remove unused import for SmartDashboard in Robot.java

--- src/main/java/org/team1126/robot/Robot.java
+++ src/main/java/org/team1126/robot/Robot.java
@@ -5,7 +5,6 @@ import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
